48d9d619c9b3f41951538ea75364aecaac9d8da4
sbelka-1703 <sbelka93@gmail.com>
2022-03-02 19:08:10 -0700

    added a chevron-down icon to the dropdown Comonents button

--- src/components/navigation/DropdownMenu.js
+++ src/components/navigation/DropdownMenu.js
@@ -18,7 +18,13 @@ function DropdownMenu() {
 
                         {/* Main Button */}
 
-                        <button className=' m-2' onClick={handleToggle} >Components</button>
+                        <button className=' m-2 flex justify-center items-center' onClick={handleToggle} >
+                            
+                            <span> Components</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mx-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+                            </svg>
+                        </button>
 
 
 
@@ -36,8 +42,8 @@ function DropdownMenu() {
                                 Twitter Side-Navbar
                             </a>
                         </Link>
-                      
-                     
+
+
 
 
                     </div>) : (<></>)}
